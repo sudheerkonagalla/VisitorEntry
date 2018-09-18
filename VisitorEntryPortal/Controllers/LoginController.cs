bc866b14cf95523f92dd4a5861125891ff4fbb18
SudheerKonagalla <Sudheerkonagalla@gmail.com>
2018-09-19 01:32:07 +0530

    Initial Setup

---
+++ VisitorEntryPortal/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisitorEntryPortal.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Login(string Username, string Password)
+        //{
+
+        //    return View();
+        //}
+        [HttpPost]
+        public ActionResult Login(FormCollection LoginForm)
+        {
+            var username = LoginForm["txtuname"];
+            var password = LoginForm["txtpswd"];
+            if(username.Equals("admin") && password.Equals("admin"))
+            {
+                Session["IsAuthenticated"] = 1;
+                Session["UserName"] = username;
+                return View("~/Views/Home/index.cshtml");
+            }
+            else
+            {
+                return RedirectToRoute("Login.cshtml");
+            }
+            
+        }
+    }
+}
